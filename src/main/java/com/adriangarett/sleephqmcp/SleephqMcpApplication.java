83717a2109ffc8f25d13562277b888c83c3fd8fe
Adrian Garett Sian <adriansian@gmail.com>
2026-05-24 20:30:02 +0800

    feat: Enable core Spring Boot features and configure application properties

--- src/main/java/com/adriangarett/sleephqmcp/SleephqMcpApplication.java
+++ src/main/java/com/adriangarett/sleephqmcp/SleephqMcpApplication.java
@@ -2,8 +2,14 @@ package com.adriangarett.sleephqmcp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableCaching
+@EnableAspectJAutoProxy
 public class SleephqMcpApplication {
 
     public static void main(String[] args) {
