7a97ea9325cf9e2bea8d8dc2cda9d612d9bf6d52
Adrian Garett Sian <adriansian@gmail.com>
2026-05-24 18:06:49 +0800

    Initial commit

---
+++ src/main/java/com/adriangarett/sleephqmcp/SleephqMcpApplication.java
@@ -0,0 +1,13 @@
+package com.adriangarett.sleephqmcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleephqMcpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SleephqMcpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adriangarett/sleephqmcp/SleephqMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adriangarett.sleephqmcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleephqMcpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
